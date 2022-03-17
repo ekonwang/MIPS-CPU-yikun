abc3dd9be23324a8f57621cf86544a7c50742db4
unknown <g1547246193@gmail.com>
2022-03-17 20:08:24 +0800

    DEBUG: constant values

--- ALU/alu.sv
+++ ALU/alu.sv
@@ -8,7 +8,7 @@ module ALU #(
     input logic [N-1:0] A,
     input logic [N-1:0] B,
     input logic [2:0]   ALUcont,
-    output logic [N-1]  result,
+    output logic [N-1:0]  result,
     output logic        zero    
 );
 always_comb begin
@@ -18,9 +18,9 @@ always_comb begin
         `ALU_OR     : result = A | B; 
         `ALU_RAND   : result = A & ~B; 
         `ALU_ROR    : result = A | ~B; 
-        `ALU_SLT    : result = A < B ? `1 : `0;
+        `ALU_SLT    : result = A < B ? '1 : '0;
         `ALU_SUB    : result = A - B;
-        default     : result = `0
+        default     : result = '0;
     endcase
 end
 assign zero = !result;
--- ALU/simtop.sv
+++ ALU/simtop.sv
@@ -7,17 +7,20 @@ u32 A, B, result;
 u3  ALUcont;
 u1  zero;
 
+ALU alu(.A, .B, .ALUcont, .result, .zero);
+
 initial begin
-    $$display("@%0t: A = %d, B = %d, ALUcont = %d, result = %d, zero = %d", $time, A, B, ALUcont, result, zero);
+    $display("@%0t: A = %d, B = %d, ALUcont = %d, result = %d, zero = %d", $time, A, B, ALUcont, result, zero);
 end
 
 initial begin
-    A = `0; B = `0; ALUcont = ALU_ADD;
-    #1 A = 32`d150; B = 32`d50; ALUcont = ALU_AND;
-    #1 A = 32`d100; B = 32`d50; ALUcont = ALU_SUB;
-    #1 A = 32`d1; B = 32`d2; ALUcont = ALU_OR;
-    #1 A = 32`d10; B = 32`d5; ALUcont = ALU_SUB;
+    A = '0; B = '0; ALUcont = `ALU_ADD;
+    #1 A = 32'd150; B = 32'd50; ALUcont = `ALU_AND;
+    #1 A = 32'd100; B = 32'd50; ALUcont = `ALU_SUB;
+    #1 A = 32'd1; B = 32'd2; ALUcont = `ALU_OR;
+    #1 A = 32'd10; B = 32'd5; ALUcont = `ALU_SUB;
+end
 endmodule
 
-// 全面性 
-// 随机性
+// complete 
+// random
