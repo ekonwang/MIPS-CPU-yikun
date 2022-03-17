f3aa5e6f16cc9dd30b4c4fa8cae8e15c3724288e
yikun wang <g1547246193@gmail.com>
2022-03-17 20:49:23 +0800

    Feature: annotation

--- ALU/alu.sv
+++ ALU/alu.sv
@@ -13,14 +13,22 @@ module ALU #(
 );
 always_comb begin
     unique case(ALUcont)
-        3'b000      : result = A & B;   // `ALU_AND    : result = A & B; 
-        3'b001      : result = A | B;   // `ALU_OR     : result = A | B; 
-        3'b010      : result = A + B;   // `ALU_ADD    : result = A + B;
+        // 3'b000      : result = A & B;  
+        // 3'b001      : result = A | B;  
+        // 3'b010      : result = A + B;   
         
-        3'b100      : result = A & ~B;  // `ALU_RAND   : result = A & ~B; 
-        3'b101      : result = A | ~B;  // `ALU_ROR    : result = A | ~B; 
-        3'b110      : result = A - B;   // `ALU_SUB    : result = A - B;
-        3'b111      : result = A < B ? '1 : '0; // `ALU_SLT    : result = A < B ? '1 : '0;
+        // 3'b100      : result = A & ~B;  
+        // 3'b101      : result = A | ~B;  
+        // 3'b110      : result = A - B;   
+        // 3'b111      : result = A < B ? '1 : '0;
+        `ALU_AND    : result = A & B; 
+        `ALU_OR     : result = A | B; 
+        `ALU_ADD    : result = A + B;
+
+        `ALU_RAND   : result = A & ~B; 
+        `ALU_ROR    : result = A | ~B; 
+        `ALU_SUB    : result = A - B;
+        `ALU_SLT    : result = A < B ? '1 : '0;
         default     : result = '0;
     endcase
 end
