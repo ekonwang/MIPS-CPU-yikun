856840f64bd2281f67945098301baab9e375a3e2
unknown <g1547246193@gmail.com>
2022-03-20 12:44:51 +0800

    new simtop

--- ALU/simtop.sv
+++ ALU/simtop.sv
@@ -2,23 +2,28 @@
 
 `include "common.svh"
 
-module simtop;
+module simtop();
 u32 A, B, result;
 u3  ALUcont;
 u1  zero;
 
-ALU alu(.A, .B, .ALUcont, .result, .zero);
+ALU ALU(.A, .B, .ALUcont, .result, .zero);
 
 initial begin
+    A = '0; B = '0; ALUcont = `ALU_ADD; 
     $display("@%0t: A = %d, B = %d, ALUcont = %d, result = %d, zero = %d", $time, A, B, ALUcont, result, zero);
-end
+    
+    #1 A = 32'd150; B = 32'd50; ALUcont = `ALU_AND; 
+    $display("@%0t: A = %d, B = %d, ALUcont = %d, result = %d, zero = %d", $time, A, B, ALUcont, result, zero);
 
-initial begin
-    A = '0; B = '0; ALUcont = `ALU_ADD;
-    #1 A = 32'd150; B = 32'd50; ALUcont = `ALU_AND;
     #1 A = 32'd100; B = 32'd50; ALUcont = `ALU_SUB;
+    $display("@%0t: A = %d, B = %d, ALUcont = %d, result = %d, zero = %d", $time, A, B, ALUcont, result, zero);
+
     #1 A = 32'd1; B = 32'd2; ALUcont = `ALU_OR;
+    $display("@%0t: A = %d, B = %d, ALUcont = %d, result = %d, zero = %d", $time, A, B, ALUcont, result, zero);
+
     #1 A = 32'd10; B = 32'd5; ALUcont = `ALU_SUB;
+    $display("@%0t: A = %d, B = %d, ALUcont = %d, result = %d, zero = %d", $time, A, B, ALUcont, result, zero);
 end
 endmodule
 
