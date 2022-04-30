0a060c19409fd1caff8bd180f3e63d5461e92a1c
yikun wang <g1547246193@gmail.com>
2022-04-30 22:30:54 +0800

    Feature: memory module

--- MultiCircle/mem.sv
+++ MultiCircle/mem.sv
@@ -5,14 +5,34 @@
 module mem(
     input u1        clk,
     input u1        we,
-    input u32       addr,
     input u32       writedata,
+    input u32       pc,
+    input u32       dataaddr,
+    input u1        iord, irwrite,
 
+    output u32      instr,
     output u32      readdata
 );
-u32     data[63:0];
-assign  readdata = data[addr[31:2]];
-always @(posedge clk) begin
-    if (we) data[addr[31:2]] <= writedata;
-end
+    u32     data[127:0]; // 512 KB total storage
+    u32     read_data, read_instr, read;
+
+    read = data[addr[31:2]];  // addr must be 4-byte aligned
+    always @(posedge clk) begin
+        if (we) data[addr[31:2]] <= writedata;
+    end
+
+    always_comb begin
+        if (iord) read_data = read;
+        else read_instr = read;
+    end
+
+    always @(posedge clk) begin 
+        if (irwrite) instr = read_instr;
+        readdata = read_data;
+    end
+
+    initial begin
+    $readmemh("bubble.dat", data, PC_start/4);
+    end
+
 endmodule
