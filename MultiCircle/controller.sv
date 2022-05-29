5817b76eb726ebaa99fa1b0bce1e82a0e8a43de5
yikun wang <g1547246193@gmail.com>
2022-05-29 15:48:09 +0800

    Update: instruction extension

--- MultiCircle/controller.sv
+++ MultiCircle/controller.sv
@@ -13,7 +13,7 @@ module controller(
     output u3   alucont
 );
     u1  pcwrite, branch;
-    u2  aluop;
+    u3  aluop;
 
     assign pcen = (branch & zero) | pcwrite;
 
@@ -37,23 +37,23 @@ endmodule
 
 module aludec(
     input u6    funct,
-    input u2    aluop,
+    input u3    aluop,
     
     output u3   alucont
 );
     always_comb begin
         unique case(aluop)
-            2'b00:  alucont <= ALU_ADD;
-            2'b01:  alucont <= ALU_SUB;
-            2'b10: 
-                unique case(funct) // RTYPE
-                    FUNCT_ADD:  alucont <= ALU_ADD;
-                    FUNCT_AND:  alucont <= ALU_AND;
-                    FUNCT_OR:   alucont <= ALU_OR;
-                    FUNCT_SLT:  alucont <= ALU_SLT;
-                    FUNCT_SUB:  alucont <= ALU_SUB;
+            ALU_NO_USE:
+                unique case(funct)  // RTYPE
+                    FUNCT_ADD  :   alucont <= ALU_ADD;
+                    FUNCT_SUB  :   alucont <= ALU_SUB;
+                    FUNCT_AND  :   alucont <= ALU_AND;
+                    FUNCT_OR   :   alucont <= ALU_OR;
+                    FUNCT_SLT  :   alucont <= ALU_SLT;
+                    default    :   alucont <= 3'bxxx;
                 endcase
-            default: alucont <= 'x;
+            default:
+                alucont <= aluop;
         endcase
     end 
 endmodule
@@ -63,16 +63,19 @@ module maindec(
     input u1    clk, reset,
     input u6    op, funct,
     
+    output u1   bne,
     output u1   pcwrite, memwrite, irwrite, regwrite,
-    output u1   alusrca, branch, iord, memtoreg, regdst,
-    output u2   alusrcb, pcsrc, aluop
+    output u1   alusrca, beq, iord, memtoreg, regdst,
+    output u2   alusrcb, pcsrc, 
+    output u3   aluop,
 );
 
-    u4  state, nextstate;
-    u15 controls;
+    u5  state, nextstate;
+    u17 controls;
 
-    assign {pcwrite, memwrite, irwrite, regwrite,
-            alusrca, branch, iord, memtoreg, regdst,
+    assign {bne, pcwrite, 
+            memwrite, irwrite, regwrite,
+            alusrca, beq, iord, memtoreg, regdst,
             alusrcb, pcsrc, aluop} = controls;
     
     always_ff @(posedge clk or posedge reset) begin 
@@ -86,50 +89,76 @@ module maindec(
             DECODE: case(op)
                     LW:         nextstate = MEMADR;
                     SW:         nextstate = MEMADR;
+                    // branch.
                     BEQ:        nextstate = BEQEX;
+                    BNE:        nextstate = BNEEX;
+                    // imm.
                     ADDI:       nextstate = ADDIEX;
+                    ANDI:       nextstate = ANDIEX;
+                    ORI:        nextstate = ORIEX;
+                    SLTI:       nextstate = SLTIEX;  
+                    // jump.                  
                     J:          nextstate = JEX;
-                    // RTYPE:      nextstate = RTYPEEX;
                     RTYPE: case(funct)
                         BUBBLE:     nextstate = FETCH;
                         default:    nextstate = RTYPEEX;
                     endcase
-                    // BUBBLE:     nextstate = FETCH;
-                    default:    nextstate = 4'bx;
+                    default:    nextstate = 5'bx;
             endcase
             MEMADR: case(op)
                     LW:         nextstate = MEMRD;
                     SW:         nextstate = MEMWR;
-                    default:    nextstate = 4'bx;
+                    default:    nextstate = 5'bx;
             endcase
             MEMRD:      nextstate = MEMWB;
             MEMWB:      nextstate = FETCH;
             MEMWR:      nextstate = FETCH;
             RTYPEEX:    nextstate = RTYPEWB;
             RTYPEWB:    nextstate = FETCH;
+            // branch.
             BEQEX:      nextstate = FETCH;
+            BNEEX:      nextstate = FETCH;
+            // imm.
             ADDIEX:     nextstate = ADDIWB;
+            ANDIEX:     nextstate = ANDIWB;
+            ORIEX:      nextstate = ORIWB;
+            SLTIEX:     nextstate = SLTIWB;
             ADDIWB:     nextstate = FETCH;
+            ANDIWB:     nextstate = FETCH;
+            ORIWB:      nextstate = FETCH;
+            SLTIWB:     nextstate = FETCH;
+            // jump.
             JEX:        nextstate = FETCH;
-            default:    nextstate = 4'bx;
+            default:    nextstate = 5'bx;
         endcase
     end
 
     always_comb begin
         unique case(state)
-            FETCH:      controls = FETCH_C;
-            DECODE:     controls = DECODE_C;
-            MEMADR:     controls = MEMADR_C;
-            MEMRD:      controls = MEMRD_C;
-            MEMWB:      controls = MEMWB_C;
-            MEMWR:      controls = MEMWR_C;
-            RTYPEEX:    controls = RTYPEEX_C;
-            RTYPEWB:    controls = RTYPEWB_C;
-            BEQEX:      controls = BEQEX_C;
-            ADDIEX:     controls = ADDIEX_C;
-            ADDIWB:     controls = ADDIWB_C;
-            JEX:        controls = JEX_C;
-            default:    controls = 15'hxxxx;
+            FETCH:      controls = {FETCH_C,    ALU_ADD};
+            DECODE:     controls = {DECODE_C,   ALU_ADD};
+            MEMADR:     controls = {MEMADR_C,   ALU_ADD};
+            MEMRD:      controls = {MEMRD_C,    ALU_ADD};
+            MEMWB:      controls = {MEMWB_C,    ALU_ADD};
+            MEMWR:      controls = {MEMWR_C,    ALU_ADD};
+            RTYPEEX:    controls = {RTYPEEX_C,  ALU_NO_USE};
+            RTYPEWB:    controls = {RTYPEWB_C,  ALU_ADD};
+            // branch instructions.
+            BEQEX:      controls = {BEQEX_C,    ALU_SUB};
+            BNEEX:      controls = {BNEEX_C,    ALU_SUB};
+
+            // imm instructions.
+            ADDIEX:     controls = {ADDIEX_C,   ALU_ADD};
+            ADDIWB:     controls = {ADDIWB_C,   ALU_ADD};
+            ANDIEX:     controls = {ADDIEX_C,   ALU_AND};
+            ANDIWB:     controls = {ADDIWB_C,   ALU_ADD};
+            ORIEX:      controls = {ADDIEX_C,   ALU_OR};
+            ORIWB:      controls = {ADDIWB_C,   ALU_ADD};
+            SLTIEX:     controls = {ADDIEX_C,   ALU_SLT};
+            SLTIWB:     controls = {ADDIWB_C,   ALU_ADD};
+
+            JEX:        controls = {JEX_C,      ALU_ADD};
+            default:    controls = 16'hxxxx;
         endcase
     end
 
